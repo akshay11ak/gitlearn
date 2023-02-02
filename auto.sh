d59527dcd38ad75d9694c7781ff0377103650a93
akshay11ak <akshay111nimbalkar@gmail.com>
2023-02-02 15:00:00 +0530

    committed

---
+++ auto.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+declare -A map
+map["gitlearn"]="gitlearn"
+git init
+git add supper.txt
+git status
+git add auto.sh
+git status
+
+git commit -m "committed"
+for i in "${!map[@]}"
+
+git remote add $i git@github.com:akshay11ak/${map[$i]}.git
+git push -u $i main
+do
+done
