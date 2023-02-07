9df30f953999dbcc1f034e875bfa3aa9f40cadd4
akshay11ak <akshay111nimbalkar@gmail.com>
2023-02-07 09:41:24 +0530

    my first  gitlearn

--- auto.sh
+++ auto.sh
@@ -1,16 +1,12 @@
 #! /bin/bash
 declare -A map
 map["gitlearn"]="gitlearn"
-git init
-git add supper.txt
-git status
-git add supper.txt
-git status
 
-git commit -m "committed"
+
+
 for i in "${!map[@]}"
 
-git remote add $i git@github.com:akshay11ak/${map[$i]}.git
-git push -u $i main
+
+git pull origin main
 do
 done
